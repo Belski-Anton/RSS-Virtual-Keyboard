b0908228f23f1dc9b7f85afafb13c5997cb700ef
Anton Belski <anton.belski@gmail.com>
2023-04-27 14:31:12 +0300

    feat:add plugins,loaders for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require(`mini-css-extract-plugin`);
 
 module.exports = {
    mode:'development',
@@ -6,5 +10,35 @@ module.exports = {
    output:{
       path:path.resolve(__dirname, 'dist'),
       filename:'bundle.js',
-   }
-}
+      assetModuleFilename:'assets/[name][ext]',
+   },
+   module:{
+    rules:[
+      {
+         test: /\.(ico|png|svg|jpg|jpeg|gif)$/i,
+         type: 'asset/resource',
+      },
+      {
+         test: /\.html$/i,
+         loader: 'html-loader'   
+      },
+      {
+         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+         type: 'asset/resource',
+       },
+       {
+         test: /\.css$/i,
+         use: [MiniCssExtractPlugin.loader,"style-loader", "css-loader"],
+       }
+    ]   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template:'./src/index.html'
+      }),
+      new CleanWebpackPlugin(),
+
+      new MiniCssExtractPlugin({
+        filename:'[name].[contenthash].css',  
+      })
+   ],
+};
